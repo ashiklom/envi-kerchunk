1efdcbcb4304d1ffc2dd33e8060f9e091de1aef5
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-10-04 16:44:18 -0400

    WIP make-envi-kerchunk
    Clean up envi-kerchunk a bit.

--- envi-zarr.py
+++
@@ -1,8 +0,0 @@
-import xarray as xr
-import rioxarray
-path = "test-data/ang20170323t202244_rdn_7000-7010.hdr"
-dx = xr.open_dataset(path.rstrip(".hdr"), engine="rasterio")
-
-dx.to_zarr("test.zarr")
-
-dxtest = xr.open_dataset("test.zarr")
--- make-envi-kerchunk.py
+++ make-envi-kerchunk.py
@@ -9,8 +9,21 @@ import fsspec
 
 fs = fsspec.filesystem("file")
 
+def string_encode(x):
+    bits = base64.b64encode(x)
+    s = str(bits).lstrip("b'").rstrip("'")
+    return f"base64:{s}"
+
 # output_file = "small-example.json"
 output_file = "big-example.json"
+
+zarray_common = {
+    "compressor": None,
+    "fill_value": None,
+    "filters": None,
+    "order": "C",
+    "zarr_format": 2
+}
 
 # rdn_path = "test-data/ang20170323t202244_rdn_7000-7010.hdr"
 rdn_path = "../sbg-uncertainty/data/isofit-test-data/medium_chunk/ang20170323t202244_rdn_7k-8k.hdr"
@@ -26,24 +39,15 @@ dat = envi.open(rdn_path)
 nsamp = int(dat.metadata["samples"])
 nlines = int(dat.metadata["lines"])
 
-def string_encode(x):
-    bits = base64.b64encode(x)
-    s = str(bits).lstrip("b'").rstrip("'")
-    return f"base64:{s}"
-
 waves = np.array(dat.metadata["wavelength"], np.float32)
 waves_b64 = string_encode(waves)
 
 waves_dict = {
     "wavelength/.zarray": ujson.dumps({
+        **zarray_common,
         "chunks": [len(waves)],
-        "compressor": None,
         "dtype": "<f4",  # Float 32
-        "fill_value": None,
-        "filters": None,
-        "order": "C",
         "shape": [len(waves)],
-        "zarr_format": 2
     }),
     "wavelength/.zattrs": ujson.dumps({
         "_ARRAY_DIMENSIONS": ["wavelength"]
@@ -55,33 +59,25 @@ lines = np.arange(nlines, dtype="<i4")
 lines_b64 = string_encode(lines)
 lines_dict = {
     "line/.zarray": ujson.dumps({
+        **zarray_common,
         "chunks": [nlines],
-        "compressor": None,
         "dtype": "<i4",
-        "fill_value": None,
-        "filters": None,
-        "order": "C",
         "shape": [nlines],
-        "zarr_format": 2
     }),
     "line/.zattrs": ujson.dumps({
         "_ARRAY_DIMENSIONS": ["line"]
     }),
-    "line/0": lines_b64,
+    "line/0": lines_b64
 }
 
 samps = np.arange(nsamp, dtype="<i4")
 samps_b64 = string_encode(samps)
 samps_dict = {
     "sample/.zarray": ujson.dumps({
+        **zarray_common,
         "chunks": [nsamp],
-        "compressor": None,
         "dtype": "<i4",
-        "fill_value": None,
-        "filters": None,
-        "order": "C",
         "shape": [nsamp],
-        "zarr_format": 2
     }),
     "sample/.zattrs": ujson.dumps({
         "_ARRAY_DIMENSIONS": ["sample"]
@@ -96,14 +92,10 @@ radiance_chunks = {
 }
 radiance_dict = {
     "radiance/.zarray": ujson.dumps({
+        **zarray_common,
         "chunks": [1, len(waves), nsamp],
-        "compressor": None,
         "dtype": "<f4",  # < = Byte order 0; f4 = data type 4
-        "fill_value": None,
-        "filters": None,
-        "order": "C",
         "shape": [nlines, len(waves), nsamp],
-        "zarr_format": 2
     }),
     "radiance/.zattrs": ujson.dumps({
         "_ARRAY_DIMENSIONS": ["line", "wavelength", "sample"]
@@ -119,14 +111,10 @@ lat_chunks = {
 }
 lat_dict = {
     "lat/.zarray": ujson.dumps({
+        **zarray_common,
         "chunks": [1, nsamp],
-        "compressor": None,
         "dtype": "<f8", # Data type 5 = 64-bit float
-        "fill_value": None,
-        "filters": None,
-        "order": "C",
         "shape": [nlines, nsamp],
-        "zarr_format": 2
     }),
     "lat/.zattrs": ujson.dumps({
         "_ARRAY_DIMENSIONS": ["line", "sample"]
@@ -139,14 +127,10 @@ lon_chunks = {
 }
 lon_dict = {
     "lon/.zarray": ujson.dumps({
+        **zarray_common,
         "chunks": [1, nsamp],
-        "compressor": None,
         "dtype": "<f8", # Data type 5 = 64-bit float
-        "fill_value": None,
-        "filters": None,
-        "order": "C",
         "shape": [nlines, nsamp],
-        "zarr_format": 2
     }),
     "lon/.zattrs": ujson.dumps({
         "_ARRAY_DIMENSIONS": ["line", "sample"]
