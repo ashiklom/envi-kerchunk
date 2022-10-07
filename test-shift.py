ea7a043416f0ba24736d91212ddfc6a265e3413d
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-10-07 21:49:49 +0000

    Test script for processing SHIFT data

---
+++ test-shift.py
@@ -0,0 +1,40 @@
+import xarray as xr
+import rioxarray
+
+dat = xr.open_dataset("reference://", engine="zarr", backend_kwargs={
+    "consolidated": False,
+    "storage_options": {"fo": "shift.json"}
+})
+
+dat.y.max()
+dat.y.min()
+
+dsub = dat.sel(x = slice(730000, 730500), y = slice(3810500, 3810000))
+%time dvals = dsub.reflectance.values
+
+
+import rasterio as rio
+
+dat.attrs["map info"]
+
+dat2 = dat
+dat2 = dat2.rio.set_crs(32610)
+dat2 = dat2.rio.transfo
+
+# Sample --> X
+# Line --> Y
+drxr = xr.open_dataset("s3://dh-shift-curated/aviris/v1/gridded/20220224_box_rfl_phase", engine="rasterio")
+
+drxr.spatial_ref.attrs
+
+drxr.y.min().values
+ycoords.min()
+drxr.y.max().values
+ycoords.max()
+
+drxr.x.min().values
+xcoords.min()
+drxr.x.max().values
+xcoords.max()
+
+drxr.spatial_ref
